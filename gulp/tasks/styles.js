e3720e5ae3ad57042e035dee9d0047c2fd24145e
lyt2015 <lyt2015@users.nonreply.github.com>
2018-11-13 22:17:29 +0800

    add responsive images to hero section

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,12 @@ const autoPrefixer = require('autoprefixer')
 const cssVars = require('postcss-simple-vars')
 const cssNested = require('postcss-nested')
 const cssImport = require('postcss-import')
+const cssMixins = require('postcss-mixins')
 
 gulp.task('styles', () => {
   return gulp
     .src('./assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssVars, cssNested, autoPrefixer]))
+    .pipe(postcss([cssImport, cssMixins, cssVars, cssNested, autoPrefixer]))
     .on('error', function(error) {
       console.log(error.toString())
       this.emit('end')

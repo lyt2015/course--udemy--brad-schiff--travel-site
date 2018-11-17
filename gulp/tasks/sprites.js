3816e01e078f74ead1c7146262e19c31d94d6d7b
lyt2015 <lyt2015@users.nonreply.github.com>
2018-11-17 19:48:42 +0800

    add gulp tasks for fine tuning svg and css generation

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp')
 const svgSprite = require('gulp-svg-sprite')
+const rename = require('gulp-rename')
 
 const config = {
   mode: {
     css: {
+      sprite: 'sprite.svg',
       render: {
         css: {
           template: './gulp/templates/sprite.css',
@@ -19,3 +21,16 @@ gulp.task('createSprite', () => {
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./temp/sprite/'))
 })
+
+gulp.task('copySpriteGraphic', ['createSprite'], () => {
+  return gulp.src('./temp/sprite/css/**/*.svg').pipe(gulp.dest('./assets/images/sprites'))
+})
+
+gulp.task('copySpriteCSS', ['createSprite'], () => {
+  return gulp
+    .src('./temp/sprite/css/*.css')
+    .pipe(rename('_sprite.css'))
+    .pipe(gulp.dest('./assets/styles/modules'))
+})
+
+gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS'])

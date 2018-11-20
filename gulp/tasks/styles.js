0d4b9b37bd991262b2a2c423c328bb35f4686728
lyt2015 <lyt2015@users.nonreply.github.com>
2018-11-20 13:26:18 +0800

    completed desktop version of header

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,12 +4,13 @@ const autoPrefixer = require('autoprefixer')
 const cssVars = require('postcss-simple-vars')
 const cssNested = require('postcss-nested')
 const cssImport = require('postcss-import')
-const cssMixins = require('postcss-mixins')
+const mixins = require('postcss-mixins')
+const hexrgba = require('postcss-hexrgba')
 
 gulp.task('styles', () => {
   return gulp
     .src('./assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssMixins, cssVars, cssNested, autoPrefixer]))
+    .pipe(postcss([cssImport, mixins, cssVars, cssNested, hexrgba, autoPrefixer]))
     .on('error', function(error) {
       console.log(error.toString())
       this.emit('end')

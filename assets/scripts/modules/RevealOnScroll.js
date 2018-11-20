c52bcce7e03cc01d9a6bc9d69836c750d56f1ffe
lyt2015 <lyt2015@users.nonreply.github.com>
2018-11-20 22:10:50 +0800

    add basics of reveal on scroll function

---
+++ assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,30 @@
+import $ from 'jquery'
+import waypoints from '../../../node_modules/waypoints/lib/noframework.waypoints'
+
+class RevealOnScroll {
+  constructor() {
+    this.itemsToReveal = $('.feature-item')
+
+    this.hideInitially()
+    this.createWaypoints()
+  }
+
+  hideInitially() {
+    this.itemsToReveal.addClass('reveal-item')
+  }
+
+  createWaypoints() {
+    this.itemsToReveal.each(function() {
+      const self = this
+      new Waypoint({
+        element: self,
+        handler: () => {
+          $(self).addClass('reveal-item--is-visible')
+        },
+        offset: '85%',
+      })
+    })
+  }
+}
+
+export default RevealOnScroll
